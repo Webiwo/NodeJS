d24866c29bf729ad54a4d026fb24796ede28c098
webiwo <webiwo360@gmail.com>
2025-03-13 20:02:31 +0100

    CORS and error handling

--- ExpressWebServer/server.js
+++ ExpressWebServer/server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 
 const PORT = 3000;
 const app = express();
 
 //custom middleware logger
 app.use(logger);
+
+//Cross Origin Resource Sharing
+const whitelist = [
+  "https://www.website.com",
+  "http://localhost:3000",
+  undefined,
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 //buit-in middleware
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +49,8 @@ app.get("/old-page(.html)?", (req, res) => {
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
